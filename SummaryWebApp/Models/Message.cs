d0650896f595be5514817eb2376af4ec23bf7861
Amogh M Cheluvaraj <Amogh.MCheluvaraj@fnf.com>
2024-11-29 15:09:37 +0530

    move message to models

---
+++ SummaryWebApp/Models/Message.cs
@@ -0,0 +1,14 @@
+﻿using Plugins;
+
+namespace SummaryWebApp.Models
+{
+    public class Message
+    {
+        public string? Text { get; set; }
+        public bool IsSent { get; set; }
+        public List<string> Texts { get; set; } = [];
+        public Topic Topic { get; set; }
+        public int PromptLen { get; set; }
+        public int CurrIndex { get; set; } = 0;
+    }
+}
